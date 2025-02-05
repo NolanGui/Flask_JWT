1297f5eb5d8ac11cc6d29850226beb29dfa786d2
Nolan Guiziou <guiziou.nolan@gmail.com>
2025-02-05 10:06:54 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
                                                                                                                                        
 # Configuration du module JWT
 app.config["JWT_SECRET_KEY"] = "Ma_clé_secrete"  # Ma clée privée
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+
 jwt = JWTManager(app)
 
 @app.route('/')
